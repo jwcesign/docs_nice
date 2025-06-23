ceaa86ab119f3fc5cf965aa1ec172182e5144cc7
qiushui7 <997179306@qq.com>
2025-06-23 16:04:36 +0800

    fix: replace slack link
    Signed-off-by: qiushui7 <997179306@qq.com>

--- src/constant.ts
+++ src/constant.ts
@@ -1,1 +1,1 @@
-export const SLACK_INVITE_LINK = 'https://join.slack.com/t/cloudpilotai/shared_invite/zt-36q9wpyol-N2X3izL~HZcf1_QSko6~6Q';
+export const SLACK_INVITE_LINK = 'https://join.slack.com/t/cloudpilotaicommunity/shared_invite/zt-37rwpf8k7-Rx4BjrhuWtk9U0MXBKYL7A';
